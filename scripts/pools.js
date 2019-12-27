f8ceb487a5abbdaa7deaa948ef9a80d6bb9b3515
slavserver <onvosnovs@gmail.com>
2019-12-27 19:07:00 +0300

    Update pools.js

--- scripts/pools.js
+++ scripts/pools.js
@@ -6,34 +6,28 @@ const pools = [
   }, {
     name: 'AltPool',
     url: 'http://ubq.altpool.pro'
-  }, {
-    name: 'BAIKALMINE',
-    url: 'http://pool.baikalmine.com/en/ubiq/'
-  }, {
+  }, 
+  {
     name: 'ZET',
     url: 'https://ubq.zet-tech.eu'
-  }, {
+  }, 
+  {
     name: 'UbiqPool.io',
     url: 'https://ubiqpool.io'
-  }, {
+  }, 
+  {
     name: 'WATTPOOL',
     url: 'https://ubiq.wattpool.net'
-  }, {
-    name: 'Ubiqpool.fr',
-    url: 'https://ubiqpool.fr'
-  }, {
+  }, 
+  {
     name: 'MyPool',
     url: 'https://ubq.mypool.online'
-  }, {
-    name: 'Mole-Pool',
-    url: 'https://ubiq.mole-pool.net'
-  }, {
-    name: 'TerraHash',
-    url: 'http://terrahash.cc'
-  }, {
+  }, 
+  {
     name: 'MaxHash',
     url: 'https://ubiqpool.maxhash.org'
-  }, {
+  },
+  {
     name: 'Pool.Sexy',
     url: 'https://mining-ubq.pool.sexy'
   }
