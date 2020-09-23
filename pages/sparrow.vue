64aefc5276674453e7c56894087223c16da9b11f
Luke Williams <luke@iquidus.io>
2020-09-23 12:21:23 +0200

    update sparrow (chrome), change sparrow page layout, only show metamask warning if metamask is detected

--- pages/sparrow.vue
+++ pages/sparrow.vue
@@ -11,30 +11,31 @@
         </b-row>
 
         <b-row class="sparrow-preview">
-          <b-col md="4" style="padding-top:160px;">
-            <div style="max-width:280px;margin: 0 auto;">
+          <b-col md="12">
+            <img src="~/assets/sparrow/sparrow.png" style="max-width:280px;">
+          </b-col>
+        </b-row>
+        <b-row class="sparrow-section-dark" style="padding-top:20px;padding-bottom:20px;">
+          <b-col md="6" sm="12" style="padding-top:40px;">
+            <div style="max-width:280px;margin: 0 auto;text-align:center;">
               <h4>Buy, store and send tokens globally</h4>
               <p>Sparrow equips you with a key vault, secure login, and token wallet - <b>everything you need to manage your digital assets</b>.</p>
             </div>
           </b-col>
-          <b-col md="4">
-            <img src="~/assets/sparrow/sparrow.png">
-          </b-col>
-          <b-col md="4" style="padding-top:160px;">
-            <div style="max-width:280px;margin: 0 auto;">
+          <b-col md="6" sm="12" style="padding-top:40px;">
+            <div style="max-width:280px;margin: 0 auto;text-align:center;">
               <h4>Explore blockchain apps</h4>
               <p>Sparrow provides the simplest yet most secure way to connect to blockchain-based applications. You are always in control when interacting on the new decentralized web</b>.</p>
             </div>
           </b-col>
         </b-row>
-
-        <b-row class="sparrow-section-dark">
+        <b-row class="sparrow-section-dark-2">
           <b-col md="12">
             <b-row style="text-align:center;">
               <h4 style="margin: 15px auto;">Supported Browsers</h4>
             </b-row>
           </b-col>
-          <b-col md="12">
+          <b-col v-if="hasMetaMask" md="12">
             <b-row style="text-align:center;">
               <b-alert show style="margin: 0 auto;">Warning: do not install Sparrow in the same browser as MetaMask, a conflict will occur.</b-alert>
             </b-row>
@@ -46,9 +47,10 @@
             </a>
           </b-col>
           <b-col md="6" style="text-align:center;padding:25px 0 25px 15px;">
-            <img src="~/assets/browsers/chrome.png" width="64px;">
-            <h6 style="margin:0;padding:0">Chrome</h6>
-            <small style="margin:0;padding:0">coming soon</small>
+            <a href="https://chrome.google.com/webstore/detail/sparrow/fbeiiganbeffhobegddakpmplljhmfnd" target="_blank" style="color:#fff">
+              <img src="~/assets/browsers/chrome.png" width="64px;">
+              <h6 style="margin:0;padding:0">Chrome</h6>
+            </a>
           </b-col>
         </b-row>
       </b-card>
@@ -58,6 +60,11 @@
 
 <script>
 export default {
-  name: 'Sparrow'
+  name: 'Sparrow',
+  computed: {
+    hasMetaMask() {
+      return window.ethereum?.isMetaMask
+    }
+  }
 }
 </script>
